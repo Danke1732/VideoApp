56ecd9972260f538a903ecb0027fee0a310c7494
Danke1732 <s0lfa1732@gmail.com>
2021-07-25 22:29:08 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
